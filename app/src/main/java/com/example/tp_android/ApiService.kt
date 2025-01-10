96e82912ee0dade266af657a9a8bfc27560598f8
Amaury Brisach <amaury.brisach@etu.unilasalle.fr>
2025-01-10 17:10:13 +0100

    Version fonctionnelle (testée) avec les images

--- app/src/main/java/com/example/tp_android/ApiService.kt
+++ app/src/main/java/com/example/tp_android/ApiService.kt
@@ -2,14 +2,15 @@ import com.example.tp_android.Entity.Product
 import retrofit2.Call
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("products/1")
     suspend fun getProduct(): Product
 
-    @GET("products/categories")
-    suspend fun getProductByCat(): Product
+    @GET("products")
+    suspend fun getProducts(): List<Product>
 
-    @GET("products/categories/1")
-    suspend fun getProductByCatId(): Product
+//    @GET("products/categories/1")
+//    suspend fun getProductByCatId(): Product
 }
--- app/src/main/java/com/example/tp_android/MainActivity.kt
+++ app/src/main/java/com/example/tp_android/MainActivity.kt
@@ -4,36 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
-import coil.compose.rememberImagePainter
-
-import androidx.compose.runtime.Composable
-
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.Modifier.Companion
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import kotlinx.coroutines.launch
 import com.example.tp_android.Entity.Product
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,26 +29,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PageProduit() {
-    val Produit = remember { mutableStateOf<Product?>(null) }
+    var product by remember { mutableStateOf<Product?>(null) }
     val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(Unit) {
         coroutineScope.launch {
-            val APIRecupProduit = RetrofitInstance.api.getProduct()
-            Produit.value = APIRecupProduit
+            try {
+                val apiProduct = RetrofitInstance.api.getProduct()
+                product = apiProduct
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
+    if (product != null) {
+        val productValue = product!!
+        Text(text = "${productValue.title}\nDescription: ${productValue.description}\nPrix: ${productValue.price} €")
 
-
-    if (Produit.value != null) {
-        val product = Produit.value!!
-        Text(text = "${product.title}\nDescription: ${product.description}\nPrix: ${product.price} €")
         Image(
-            painter = rememberAsyncImagePainter(model = ImageRequest.Builder(LocalContext.current)
-                .data("https://example.com/image.jpg")),
-            contentDescription = product.title,
-            modifier = Modifier.size(150.dp).fillMaxWidth(),
+            painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(productValue.image)
+                    .crossfade(true)
+                    .build()
+            ),
+            contentDescription = if (productValue.title != null) productValue.title else "Image du produit",
+            modifier = Modifier
+                .size(150.dp)
+                .fillMaxWidth(),
             contentScale = ContentScale.Crop
         )
     } else {
